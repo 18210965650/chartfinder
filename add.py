61a0f7c292221cc9b574c24c9a0adab78f57649e
18210965650 <18210965650@163.com>
2020-02-19 23:24:55 +0800

    改版1，对页面折叠

--- add.py
+++ add.py
@@ -20,28 +20,55 @@ def match(dir) :
         return "精密进近地形图:"
     else :
         return "其他:"
-fir = "ZSHA"
-por = "ZSAM"
+fir = "ZYSH"
 name = "厦门高崎国际机场"
-str = fir+"\\"+por
+str = fir
 mulu = []
 fil = []
 filename = "0"
 extension = "0"
-print("<h4>"+por+"</h4>")
-print("<p class=\"tag\">"+name+"</p>")
-for root,dirs,files in os.walk("F:\\Users\\18210\\Documents\\eAIP\\chartfinder\\"+str) : 
-    for dirn in dirs :
-        print("<p class=\"tag\">"+match(dirn)+"</p>")
-        for n,t,pdfs in os.walk("F:\\Users\\18210\\Documents\\eAIP\\chartfinder\\"+str+"\\"+dirn) :
-            cout = 0
-            for pdf in pdfs :
-                filename,extension = os.path.splitext(pdf)
-                cout = cout + 1
-                if cout == 2 :
-                    print(count+"&nbsp;&nbsp;<a href=\""+"./"+por+"/"+dirn+"/"+pdf+"\">"+filename+"</a></p>")
-                    cout = 0
-                else :
-                    count = "<p class=\"tag\"><a href=\""+"./"+por+"/"+dirn+"/"+pdf+"\">"+filename+"</a>"
-            if cout == 1 :
-                print(count+"</p>")
+coutd = 0
+cs = 0
+dira = os.listdir("F:\\Users\\18210\\Documents\\eAIP\\chartfinder\\"+str)
+names = dict()
+for a in dira :
+    name = input(a)
+    names.update({a:name})
+for dirs in  dira:
+    coutd = coutd+1
+    if cs == 0 :
+        print("<div class=\"gallery\">")
+        cs = 1
+    if coutd == 7 :
+        print("</div>")
+        print("<div class=\"gallery\">")
+        coutd = 0
+    str = fir+"//"+dirs
+    print("<div class=\"gz1\">")
+    print("<div class=\"layui-collapse\">")
+    print("<div class=\"layui-colla-item\">")
+    print("<h4 class=\"layui-colla-title\">"+dirs+"/"+names.get(dirs)+"</h4>")
+    print("<div class=\"layui-colla-content\">")
+    dirt = dirs
+    for root,dirs,files in os.walk("F:\\Users\\18210\\Documents\\eAIP\\chartfinder\\"+str) : 
+        for dirn in dirs :
+            print("<p class=\"tag\">"+match(dirn)+"</p>")
+          
+            for n,t,pdfs in os.walk("F:\\Users\\18210\\Documents\\eAIP\\chartfinder\\"+str+"\\"+dirn) :
+                cout = 0
+                for pdf in pdfs :
+                    filename,extension = os.path.splitext(pdf)
+                    cout = cout + 1
+                    if cout == 2 :
+                        print(count+"&nbsp;&nbsp;<a href=\""+"./"+dirt+"/"+dirn+"/"+pdf+"\">"+filename+"</a></p>")
+                        cout = 0
+                    else :
+                        count = "<p class=\"tag\"><a href=\""+"./"+dirt+"/"+dirn+"/"+pdf+"\">"+filename+"</a>"
+                if cout == 1 :
+                    print(count+"</p>")
+    print("</div>")
+    print("</div>")
+    print("</div>")
+    print("</div>")
+if coutd != 7 :
+    print("</div>")
